993d2dd4df3704aac6d4811d465171494ab8c44c
Souchy <robyn.g@hotmail.fr>
2022-10-20 09:02:23 -0400

    glaceon uis

---
+++ Glaceon/ui/amethyst/ui_chat.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func close_on_close_button_up():
+	hide();
+	pass # Replace with function body.
