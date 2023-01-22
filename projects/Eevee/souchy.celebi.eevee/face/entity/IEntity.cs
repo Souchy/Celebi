25bbd0f9a1d1958225300c005ded9eeedffcc714
Souchy <robyn.g@hotmail.fr>
2023-01-22 11:08:30 -0500

    VERY IMPORTANT Observer event in IEntity to update UIs when data changes

--- projects/Eevee/souchy.celebi.eevee/face/entity/IEntity.cs
+++ projects/Eevee/souchy.celebi.eevee/face/entity/IEntity.cs
@@ -1,9 +1,48 @@
-﻿using souchy.celebi.eevee.face.util;
+﻿using Godot;
+using souchy.celebi.eevee.face.util;
 
 namespace souchy.celebi.eevee.face.entity
 {
     public interface IEntity : IDisposable
     {
         public IID entityUid { get; init; }
+
+
+        //      (ObserverPattern)
+        // UIs listen to this signal to update display
+        // signal Changed(string propertyPath, object entity, object oldValue)
+        //      ex path in spellmodel: "properties.cooldown"
+
+        //[Signal]
+        public delegate void OnChanged(object entity, string propertyPath, object oldValue);
+
+        public event OnChanged Changed;
+        //{ 
+            //add
+            //{
+                
+            //}
+            //remove
+            //{
+
+            //}
+        //}
+
+        //
+        // Summary:
+        //     Represents the method that handles the Godot.Range.Changed event of a Godot.Range
+        //     class.
+        //public event Action Changed
+        //{
+        //    add
+        //    {
+        //        Connect(SignalName.Changed, Callable.From(value));
+        //    }
+        //    remove
+        //    {
+        //        Disconnect(SignalName.Changed, Callable.From(value));
+        //    }
+        //}
+
     }
 }
