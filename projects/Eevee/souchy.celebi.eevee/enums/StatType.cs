7e6bfb305acb02bdeb2548002672d848522e2d30
Souchy <robyn.g@hotmail.fr>
2023-01-25 22:40:03 -0500

    StatType added States+Regen. Organize regions
    Also LifeMax can be eroded and buffed etc
    so we really need InitialLife somewhere
    as well as CurrentMaxLife + MaxLifeBuffs

--- projects/Eevee/souchy.celebi.eevee/enums/StatType.cs
+++ projects/Eevee/souchy.celebi.eevee/enums/StatType.cs
@@ -1,27 +1,12 @@
 ﻿namespace souchy.celebi.eevee.enums
 {
+
     public enum StatValueType
     {
         Simple,
         Detailed,
         Bool,
         Resource
-    }
-
-    public enum ResourceType
-    {
-        Life,
-        Mana,
-        Movement,
-        Rage,
-        Summons,
-
-        // TODO ResourceMax
-        LifeMax,
-        ManaMax,
-        MovementMax,
-        RageMax,
-        SummonsMax,
     }
 
     public enum ElementType
@@ -34,6 +19,7 @@
 
     public enum StatType
     {
+        #region Resources & Limits & Regens
         Life,
         Mana,
         Movement,
@@ -47,10 +33,26 @@
         RageMax,
         SummonsMax,
 
-        Range,
-        Speed,
+        LifeRegen, // -1 = full every turn, 0 = no regen, 1 = 1/turn, 0.25 = 1 per 4 turns // string to parse for different regens
+        ManaRegen,
+        MovementRegen,
+        RageRegen,
+        #endregion
 
-        // 
+
+        #region Affinities & Resistances
+        // elemental
+        FireAffinity,
+        WaterAffinity,
+        AirAffinity,
+        EarthAffinity,
+
+        FireResistance,
+        WaterResistance,
+        AirResistance,
+        EarthResistance,
+
+        // others
         GlobalAffinity,
         MeleeAffinity,
         DistanceAffinity,
@@ -66,18 +68,81 @@
         TrapResistance,
         GlyphResistance,
         PoisonResistance,
+        #endregion
 
-        // elemental
-        FireAffinity,
-        WaterAffinity,
-        AirAffinity,
-        EarthAffinity,
+        #region Other
+        Range,
+        Speed, // Initiative in timeline
+        #endregion
 
-        FireResistance,
-        WaterResistance,
-        AirResistance,
-        EarthResistance,
+        #region States ?
+        Visible,
+        Ghosted, // phasing // is 30% opacity, unlocks line of sight, but blocks movement
 
+        Flying, // slash Hovering
+        Underground,
 
+        Drenched, // wet
+        Shocked,
+        Hot, // burning
+        Grounded, // Muddy
+
+        Rooted, // can't translate (dash/push/pull)
+        Gravity, // can't teleport
+        Heavy, // carry/throw
+        Unmoveable, // everything 
+
+        Carrying,
+        Carried,
+
+        Pacifist
+        #endregion
     }
+
+    public enum ResourceType
+    {
+        Life = StatType.Life,
+        Mana = StatType.Mana,
+        Movement = StatType.Movement,
+        Rage = StatType.Rage,
+        Summons = StatType.Summons,
+
+        // TODO ResourceMax
+        //LifeMax = StatType.LifeMax,
+        //ManaMax = StatType.ManaMax,
+        //MovementMax = StatType.MovementMax,
+        //RageMax = StatType.RageMax,
+        //SummonsMax = StatType.SummonsMax,
+
+        //// -1 = full every turn, 0 = no regen, 1 = 1/turn, 0.25 = 1 per 4 turns // string to parse for different regens
+        //LifeRegen = StatType.LifeRegen, 
+        //ManaRegen = StatType.ManaRegen,
+        //MovementRegen = StatType.MovementRegen,
+        //RageRegen = StatType.RageRegen
+    }
+
+    public enum StateType
+    {
+        Visible = StatType.Visible,
+        Ghosted, // phasing // is 30% opacity, unlocks line of sight, but blocks movement
+
+        Flying, // slash Hovering
+        Underground,
+
+        Drenched, // wet
+        Shocked, // 
+        Hot, // burning
+        Grounded, // Muddy
+
+        Rooted, // can't translate (dash/push/pull)
+        Gravity, // can't teleport
+        Heavy, // carry/throw
+        Unmoveable, // everything 
+
+        Carrying,
+        Carried,
+
+        Pacifist
+    }
+
 }
