8f17673ad85e2b4fc6911563a508aef102c24cba
Souchy <robyn.g@hotmail.fr>
2023-01-28 17:27:30 -0500

    Start impl EffectDirectDamage + EffectHeal

--- projects/Eevee/souchy.celebi.eevee/impl/shared/effects/res/EffectDirectDamage.cs
+++ projects/Eevee/souchy.celebi.eevee/impl/shared/effects/res/EffectDirectDamage.cs
@@ -1,9 +1,65 @@
-﻿using souchy.celebi.eevee.face.objects;
+﻿using souchy.celebi.eevee.enums;
+using souchy.celebi.eevee.face.objects;
+using souchy.celebi.eevee.face.objects.compiledeffects;
+using souchy.celebi.eevee.face.objects.controllers;
+using souchy.celebi.eevee.face.objects.stats;
+using souchy.celebi.eevee.face.util;
+using souchy.celebi.eevee.face.values;
 using souchy.celebi.eevee.impl.objects;
+using souchy.celebi.eevee.impl.objects.compiledeffects;
 
 namespace souchy.celebi.eevee.face.shared.effects.res
 {
     public class EffectDirectDamage : Effect, IEffectDirectDamage
     {
+        public IValue<ElementType> element { get; set; }
+        public IValue<int> Value { get; set; }
+
+        public override ICompiledEffect compile(IFight fight, IID source, IID targetCell)
+        {
+            var creaSource = fight.creatures.Get(source);
+            var creaTarget = fight.board.getCreatureOnCell(targetCell);
+            if (creaSource == null || creaTarget == null) return null;
+
+            var sourceStats = creaSource.GetStats();
+            var targetStats = creaTarget.GetStats();
+
+            var dist = creaSource.position.distanceManhattan(creaTarget.position);
+
+            // apply affinities + resistances
+            IStatSimple aff = sourceStats.get<IStatSimple>(element.Value.GetAffinity());
+            IStatSimple affg = sourceStats.get<IStatSimple>(StatType.GlobalDamageAffinity);
+            IStatSimple affdist;
+
+            IStatSimple res = targetStats.get<IStatSimple>(element.Value.GetResistance());
+            IStatSimple resg = targetStats.get<IStatSimple>(StatType.GlobalDamageResistance);
+            IStatSimple resdist;
+
+            // distance
+            if (dist > 1)
+            {
+                affdist = sourceStats.get<IStatSimple>(StatType.DistanceAffinity);
+                resdist = targetStats.get<IStatSimple>(StatType.DistanceResistance);
+            } 
+            // melee
+            else
+            {
+                affdist = sourceStats.get<IStatSimple>(StatType.MeleeAffinity);
+                resdist = targetStats.get<IStatSimple>(StatType.MeleeResistance);
+            }
+
+            var damage = this.Value.Value; 
+
+            damage *= (100 + aff.Value) / 100;
+            damage *= (100 + affg.Value) / 100;
+            damage *= (100 + affdist.Value) / 100;
+
+            damage *= (100 - res.Value) / 100;
+            damage *= (100 - resg.Value) / 100;
+            damage *= (100 - resdist.Value) / 100;
+
+            var compiled = new CompiledDamage(damage);
+            return compiled;
+        }
     }
 }
--- projects/Eevee/souchy.celebi.eevee/impl/shared/effects/res/EffectHeal.cs
+++ projects/Eevee/souchy.celebi.eevee/impl/shared/effects/res/EffectHeal.cs
@@ -1,11 +1,60 @@
-﻿using souchy.celebi.eevee.face.objects;
+﻿using souchy.celebi.eevee.enums;
+using souchy.celebi.eevee.face.objects;
+using souchy.celebi.eevee.face.objects.compiledeffects;
+using souchy.celebi.eevee.face.objects.controllers;
+using souchy.celebi.eevee.face.objects.stats;
+using souchy.celebi.eevee.face.util;
 using souchy.celebi.eevee.face.values;
 using souchy.celebi.eevee.impl.objects;
+using souchy.celebi.eevee.impl.objects.compiledeffects;
+using souchy.celebi.eevee.impl.stats;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace souchy.celebi.eevee.face.shared.effects.res
 {
     public class EffectHeal : Effect, IEffectHeal
     {
-        public IValue<int> Value { get; set; }
+        public IValue<ElementType> element { get; set; }
+        public IValue<int> Value { get; set; } 
+
+        public override ICompiledEffect compile(IFight fight, IID source, IID targetCell)
+        {
+            var creaSource = fight.creatures.Get(source);
+            var creaTarget = fight.board.getCreatureOnCell(targetCell);
+            if (creaSource == null || creaTarget == null) return null;
+            var sourceStats = creaSource.GetStats();
+            var targetStats = creaTarget.GetStats();
+            var dist = creaSource.position.distanceManhattan(creaTarget.position);
+
+
+            // apply affinities + resistances
+            IStatSimple aff = sourceStats.get<IStatSimple>(element.Value.GetAffinity());
+            IStatSimple affh = sourceStats.get<IStatSimple>(StatType.HealAffinity);
+            IStatSimple affdist;
+
+            IStatSimple resh = targetStats.get<IStatSimple>(StatType.HealResistance);
+
+            // distance
+            if (dist > 1)
+                affdist = sourceStats.get<IStatSimple>(StatType.DistanceAffinity);
+            // melee
+            else
+                affdist = sourceStats.get<IStatSimple>(StatType.MeleeAffinity);
+
+            var heal = Value.Value;
+
+            heal *= (100 + aff.Value) / 100;
+            heal *= (100 + affh.Value) / 100;
+            heal *= (100 + affdist.Value) / 100;
+            
+            heal *= (100 - resh.Value) / 100;
+
+
+            IStatSimple currentLife = targetStats.get<IStatSimple>(StatType.Life);
+            var newLife = new StatSimple(currentLife.Value + heal);
+            var compiled = new CompiledEffectStat(StatType.Life, newLife);
+            return compiled;
+        }
     }
 }
