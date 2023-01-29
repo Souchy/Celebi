7cda68d81a26b1698aa2c046c15804361986b3fd
Souchy <robyn.g@hotmail.fr>
2023-01-29 01:27:45 -0500

    Update Trap.cs

--- projects/Eevee/souchy.celebi.eevee/impl/objects/statuses/Trap.cs
+++ projects/Eevee/souchy.celebi.eevee/impl/objects/statuses/Trap.cs
@@ -7,9 +7,9 @@ namespace souchy.celebi.eevee.interfaces.statuses
 {
     public class Trap : ITrap
     {
-        public IID fightUid { get; init; }
+        public IID entityUid { get; set; }
+        public IID fightUid { get; set; }
         public IID modelUid { get; set; }
-        public IID entityUid { get; init; }
 
         public IID sourceSpell { get; set; }
         public IID sourceCreature { get; set; }
