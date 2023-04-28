83d9e13e099fc38b17da95ac6d8b2132e270fa59
Souchy <robyn.g@hotmail.fr>
2023-04-28 14:10:11 -0400

    WIP signin/signup components

---
+++ projects/web/Jolteon/src/auth2/signin/signin.ts
@@ -0,0 +1,33 @@
+
+
+export class Signin {
+    /**
+     * nvm, we dont do usernames, only emails ////can be email or username
+     */
+    public identifier: string;
+    /**
+     * pass
+     */ 
+    public password: string;
+
+    constructor() {
+
+    }
+
+    public clickSignin() {
+
+    }
+
+    public clickSigninGoogle() {
+
+    }
+
+    public clickSigninTwitter() {
+
+    }
+
+    public clickSigninFacebook() {
+
+    }
+
+}
---
+++ projects/web/Jolteon/src/auth2/signup/signup.ts
@@ -0,0 +1,37 @@
+
+
+export class Signup {
+    /**
+     * 
+     */
+    public email: string;
+    /**
+     * 
+     */
+    public displayName: string;
+    /**
+     * pass
+     */ 
+    public password: string;
+
+    constructor() {
+
+    }
+
+    public clickSignup() {
+
+    }
+
+    public clickSignupGoogle() {
+
+    }
+
+    public clickSignupTwitter() {
+
+    }
+
+    public clickSignupFacebook() {
+
+    }
+
+}
