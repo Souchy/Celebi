ebb7382f4231136c2cddb970fab77a79a282201e
Souchy <robyn.g@hotmail.fr>
2023-05-02 08:17:18 -0400

    Espeon auth goes through Spark

---
+++ projects/game/Espeon/souchy.celebi/espeon/implementation/net/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Espeon.souchy.celebi.espeon.implementation.net
+{
+    public class AuthController
+    {
+
+        public void handleSignin(SigninPacket p)
+        {
+            // call spark
+            // ask it to check that the token is valid
+            // return account info if it is
+        }
+
+        public void handleSignout()
+        {
+
+        }
+
+    }
+
+
+    public class SigninPacket
+    {
+        public string token { get; set; }
+    }
+
+
+}
