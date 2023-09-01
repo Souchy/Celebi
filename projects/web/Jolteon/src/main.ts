b7755b0a9795dd63e1c1cbd5046e5cc80dfb20c4
Souchy <robyn.g@hotmail.fr>
2023-09-01 17:18:54 -0400

    Jolteon use # hashtag url

--- projects/web/Jolteon/src/main.ts
+++ projects/web/Jolteon/src/main.ts
@@ -13,7 +13,7 @@ Aurelia
 	// .register(RouterConfiguration)
 	// To use HTML5 pushState routes, replace previous line with the following
 	// customized router config.
-	.register(RouterConfiguration.customize({ useUrlFragmentHash: false }))
+	.register(RouterConfiguration.customize({ useUrlFragmentHash: true }))
 	.register(
 		I18nConfiguration.customize((options) => {
 			options.initOptions = {
